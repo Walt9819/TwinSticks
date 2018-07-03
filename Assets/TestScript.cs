203f035e1d0d1c5566fe90ab5b431015c62ac95a
walt22 <walter22r@icloud.com>
2018-07-03 17:11:40 -0500

    Initial commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("You find the sausage");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
